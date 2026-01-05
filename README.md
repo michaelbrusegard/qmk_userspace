e99d6555b7c560aa244dae61c8633d021da583e2
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-05 02:13:57 +0100

    move corne back to old place

--- README.md
+++ README.md
@@ -58,17 +58,3 @@ This can also be used to control which fork is used, though only upstream `qmk_f
 1. (First time only) `git submodule add https://github.com/qmk/qmk_firmware.git`
 1. (To update) `git submodule update --init --recursive`
 1. Commit your changes to your userspace repository
-
-## Commands
-
-Setup QMK:
-
-```bash
-qmk setup
-```
-
-Compile corne:
-
-```bash
-qmk compile -kb crkbd/rev4_1/standard -km corne
-```
--- keyboards/lily58/keymaps/default/keymap.c
+++ keyboards/lily58/keymaps/default/keymap.c
@@ -1,11 +1,20 @@
 #include QMK_KEYBOARD_H
 
-enum layer_number {
-  _QWERTY = 0,
+enum layers {
+  _QWERTY,
   _LOWER,
   _RAISE,
   _ADJUST,
 };
+
+#define HOME_A LCTL_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LSFT_T(KC_D)
+#define HOME_F LGUI_T(KC_F)
+#define HOME_J RGUI_T(KC_J)
+#define HOME_K RSFT_T(KC_K)
+#define HOME_L RALT_T(KC_L)
+#define HOME_SCLN RCTL_T(KC_SCLN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -97,53 +106,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
-
-//SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
-#ifdef OLED_ENABLE
-
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master())
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-  return rotation;
-}
-
-// When you add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
-const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
-
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
-
-bool oled_task_user(void) {
-  if (is_keyboard_master()) {
-    // If you want to change the display of OLED, you need to change here
-    oled_write_ln(read_layer_state(), false);
-    oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
-    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
-    //oled_write_ln(read_host_led_state(), false);
-    //oled_write_ln(read_timelog(), false);
-  } else {
-    oled_write(read_logo(), false);
-  }
-    return false;
-}
-#endif // OLED_ENABLE
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-#ifdef OLED_ENABLE
-    set_keylog(keycode, record);
-#endif
-    // set_timelog();
-  }
-  return true;
+    if (record->event.pressed) {
+        if ((keycode == KC_B || keycode == KC_N) &&
+            (get_mods() & MOD_MASK_CTRL) &&
+            (get_mods() & MOD_MASK_SHIFT) &&
+            (get_mods() & MOD_MASK_ALT) &&
+            (get_mods() & MOD_MASK_GUI)) {
+            reset_keyboard();
+            return false;
+        }
+
+    }
+    return true;
 }
