a6381c12411288c77690de50096fba8bdc905c5a
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-05 03:50:19 +0100

    Revert "create lily layout"
    This reverts commit 31e8a660a8f9029e8a04c82ddbf00f1b7888d862.

--- keyboards/crkbd/rev4_1/standard/keymaps/corne/keymap.c
+++ keyboards/crkbd/rev4_1/standard/keymaps/corne/keymap.c
@@ -1,10 +1,10 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-  _BASE,
+  _BAS,
   _NUM,
   _SYM,
-  _FN
+  _FUN
 };
 
 #define HOME_A LCTL_T(KC_A)
@@ -17,7 +17,7 @@ enum layers {
 #define HOME_SCLN RCTL_T(KC_SCLN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT_split_3x6_3_ex2(
+  [_BAS] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LEFT,    KC_UP,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -53,11 +53,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   ),
 
-  [_FN] = LAYOUT_split_3x6_3_ex2(
+  [_FUN] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
      KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-     KC_TRNS, KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, KC_NO,   KC_NO,      KC_NO,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
+     KC_TRNS, KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI,   KC_NO,   KC_NO,      KC_NO,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
      KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
